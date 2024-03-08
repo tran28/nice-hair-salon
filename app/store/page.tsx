dd639628646f7de02513f726b5d4d0d66392ce7c
Matthew Tran <tran28@my.yorku.ca>
2024-03-08 00:33:43 -0500

    UPDATE: store hours

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -10,7 +10,7 @@ export default function store() {
       <div className="flex flex-col gap-3">
         <h1 className="text-2xl font-semibold tracking-tight">Hours of Operation</h1>
         <div className="text-lg font-light flex flex-row gap-4">
-          <p>Monday to Friday</p>
+          <p>Monday to Sunday</p>
           <p>9 a.m. to 8 p.m.</p>
         </div>
       </div>
